1885f9cca2173cd2b90d5f69488a15aa9f985086
AlonsoAliaga <36780081+AlonsoAliaga@users.noreply.github.com>
2025-08-27 10:12:35 -0500

    Updated index.html

--- docs/skript.js
+++ docs/skript.js
@@ -1,3 +1,19 @@
 window.addEventListener("message",event=>{
-    console.log(event.data);
+    let data = event.data;
+    if(data && data.download) {
+        console.log(data.download);
+        data.download.generateAsync({type:"blob"})
+            .then(function(blob) {
+              saveAsLegacy(blob, "test.zip");
+            });
+    }
+    function saveAsLegacy(content, filename) {
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(content);
+      link.download = filename;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      URL.revokeObjectURL(link.href); // Clean up
+    }
 })
